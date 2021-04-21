0cd2adb3548a7dccfe5082b83d397de4cb3b428c
KOH <52293984+udoncyber@users.noreply.github.com>
2021-04-21 17:23:04 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,12 +6,12 @@ sudo add-apt-repository ppa:ondrej/php
 sudo apt-get install php
 sudo apt-get install unzip
 
-wget https://raw.githubusercontent.com/abanobem/Flussonic-18.12/master/flussonic-python_18.11_amd64.deb
-wget https://raw.githubusercontent.com/abanobem/Flussonic-18.12/master/flussonic-ffmpeg-base_4.6_amd64.deb
-wget https://raw.githubusercontent.com/abanobem/Flussonic-18.12/master/flussonic-ffmpeg_4.6_amd64.deb
-wget https://raw.githubusercontent.com/abanobem/Flussonic-18.12/master/flussonic-erlang_21.1.4_amd64.deb
-wget https://raw.githubusercontent.com/abanobem/Flussonic-18.12/master/flussonic_18.12.1_amd64.deb
-wget https://raw.githubusercontent.com/abanobem/Flussonic-18.12/master/hack.zip
+wget https://raw.githubusercontent.com/udoncyber/000/master/flussonic-python_18.11_amd64.deb
+wget https://raw.githubusercontent.com/udoncyber/000/master/flussonic-ffmpeg-base_4.6_amd64.deb
+wget https://raw.githubusercontent.com/udoncyber/000/master/flussonic-ffmpeg_4.6_amd64.deb
+wget https://raw.githubusercontent.com/udoncyber/000/master/flussonic-erlang_21.1.4_amd64.deb
+wget https://raw.githubusercontent.com/udoncyber/000/master/flussonic_18.12.1_amd64.deb
+wget https://raw.githubusercontent.com/udoncyber/000/master/hack.zip
 
 unzip hack.zip
 dpkg -i flussonic-python_18.11_amd64.deb
